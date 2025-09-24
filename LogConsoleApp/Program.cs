190c445f254b13934d526bf7edac9094643ba184
Adarsh Kaul <adarsh.kaul@atidan.com>
2025-09-24 12:08:01 +0530

    Add project files.

---
+++ LogConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using LoggerStdClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.Instance.LogError(LogLevelEnum.Info,"hello");
+        Logger.Instance.LogError(LogLevelEnum.Error,"BYe");
+     
+    }
+}
---
+++ LoggerStdClass/Logger.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Reflection.Emit;
+
+namespace LoggerStdClass
+{
+    public sealed class Logger
+    {
+        private static readonly Lazy<Logger> _lazy = new(() => new Logger());
+        public static Logger Instance => _lazy.Value;
+        private Logger() { }
+        
+        public void LogError(LogLevelEnum Level,string message)
+        {
+            {
+                string logEntry = $"[{DateTime.Now}] [{Level}] {message}";
+                Console.WriteLine(logEntry);
+            }
+        }
+    }
+}
+
